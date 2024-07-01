c27a19aef8a0b0eaed2439a929755326ae79f7e0
mickeymarse <mmserrapica@tutanota.com>
2024-07-01 16:16:39 +0100

    added function which automates GET requests

--- pingmysite/core/views.py
+++ pingmysite/core/views.py
@@ -1,30 +1,77 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# import requests
+# from bs4 import BeautifulSoup
+
+# def get_r(site):
+#     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+#     LANGUAGE = "en-US,en;q=0.5"
+#     session = requests.Session()
+#     session.headers['User-Agent'] = USER_AGENT
+#     session.headers['Accept-Language'] = LANGUAGE
+#     session.headers['Content-Language'] = LANGUAGE
+#     site = site.replace(' ', '+')
+#     r = session.get(f'{site}')
+#     return r
+
+
+# def home(request):
+#     data = None
+#     if 'site' in request.GET:
+#         # fetch
+#         site = request.GET.get('site')
+#         r = get_r(site)
+#         html_content = r.text
+#         status = r.status_code
+#         soup = BeautifulSoup(html_content, 'html.parser')
+#         data = dict()
+#         data['status'] = status
+#         data['heading'] = soup.find('h1').text
+#         print(f'{data}')
+#     return render(request, 'core/home.html', {'data': data})
+
 import requests
 from bs4 import BeautifulSoup
+from django.shortcuts import render
+from django.conf import settings
+from django.utils.timezone import now
+import threading
+import time
 
-def get_r(site):
+def automated_get_request():
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     LANGUAGE = "en-US,en;q=0.5"
-    session = requests.Session()
-    session.headers['User-Agent'] = USER_AGENT
-    session.headers['Accept-Language'] = LANGUAGE
-    session.headers['Content-Language'] = LANGUAGE
-    site = site.replace(' ', '+')
-    r = session.get(f'{site}')
-    return r
+    sites = ['https://tarmarapi.fly.dev/', 'https://metrak.fly.dev/', 'https://mickeymarse.dev/']
+    data_list = []
 
-
-def home(request):
-    data = None
-    if 'site' in request.GET:
-        # fetch
-        site = request.GET.get('site')
-        r = get_r(site)
+    for site in sites:
+        session = requests.Session()
+        session.headers['User-Agent'] = USER_AGENT
+        session.headers['Accept-Language'] = LANGUAGE
+        session.headers['Content-Language'] = LANGUAGE
+        r = session.get(f'{site}')
         html_content = r.text
         status = r.status_code
         soup = BeautifulSoup(html_content, 'html.parser')
         data = dict()
+        data['site'] = site
         data['status'] = status
-        data['heading'] = soup.find('h1').text
-        print(f'{data}')
-    return render(request, 'core/home.html', {'data': data})
+        data['heading'] = soup.find('p').text
+        data_list.append(data)
+        print(data_list)
+
+    return data_list
+
+def home(request):
+    data_list = automated_get_request()
+    return render(request, 'core/home.html', {'data_list': data_list})
+
+
+def start_automated_get_request():
+    interval = getattr(settings, 'AUTOMATED_GET_REQUEST_INTERVAL', 10)
+    while True:
+        automated_get_request()
+        time.sleep(interval)
+
+# Start the automated GET request in a separate thread
+threading.Thread(target=start_automated_get_request, daemon=True).start()
+
--- pingmysite/pingmysite/settings.py
+++ pingmysite/pingmysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTOMATED_GET_REQUEST_INTERVAL = 10
+
 
 # Application definition
 
